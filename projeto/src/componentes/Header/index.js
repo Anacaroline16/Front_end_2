2180291bc247117567b0b65ebdd9d9723650ccc2
Anacaroline16 <caroline.cordeiro@estudante.ifro.edu.br>
2024-11-22 07:39:29 -0400

    atualizando

--- projeto/src/componentes/Header/index.js
+++ projeto/src/componentes/Header/index.js
@@ -1,15 +1,23 @@
-import Link from "next/link"
-import style from "./header.module.css"
+'use client';
+import Link from "next/link";
+import { useState } from "react";
+import style from "./header.module.css";
+
 export default function Header (){
+    const [showMenu, setShowMenu] = useState(true)
     return(
         <header>
-            <nav>
-                <ul >
-                    <li className={style.li}><Link href='/' > Home</Link></li>
-                    <li className={style.li}><Link href='/sobre' > Sobre</Link></li>
-                    <li className={style.li}><Link href='/contato' > Contato</Link></li>
-                </ul>
-            </nav>
+            { <button onClick={()=>setShowMenu(!showMenu)}>Clique</button>     /*  Colocar um icone   */}
+            {
+                showMenu &&     // gfff
+                <nav>
+                    <ul >
+                        <li className={style.li}><Link href='/' > Home</Link></li>
+                        <li className={style.li}><Link href='/sobre' > Sobre</Link></li>
+                        <li className={style.li}><Link href='/contato' > Contato</Link></li>
+                    </ul>
+                </nav>
+            }
         </header>
     )
 }
