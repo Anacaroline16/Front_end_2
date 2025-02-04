7b719a03452de16ebfe9a9bad02dcf27c86c2f4d
Anacaroline16 <caroline.cordeiro@estudante.ifro.edu.br>
2025-02-04 10:06:58 -0400

    Deus nos ajude!

--- trabalho_4/src/app/componentes/Header/index.js
+++ trabalho_4/src/app/componentes/Header/index.js
@@ -1,9 +1,18 @@
 'use client';
-
+import Link from "next/link";
+import style from "./header.module.css";
+import Image from "next/image";
 
 
 export default function Header(){
     return(
-
+        <header className={style.header}>
+            <div className={style.divMenu}>
+                <div className={style.divLogo}>
+                    <Image className={style.imagemLogo} src='/imagens/CliniSys_transparent-.png' alt="Logo da pagina" width={100} height={70}/>
+                </div>
+            </div>
+        </header>
     )
+        
 }
--- trabalho_4/src/app/page.js
+++ trabalho_4/src/app/page.js
@@ -4,6 +4,11 @@ import "./globals.css";
 
 export default function Home() {
   return(
-    <h1>Pagina principal</h1>
-  )
+    <div className={styles.container_page}>    
+      <h1 className={styles.container_page_titulo}>Bem vindo a CliniSys </h1>
+      <p className={styles.container_page_texto}>Aqui, sua saúde vem em primeiro lugar. Contamos com uma equipe experiente e preparada para oferecer um atendimento acolhedor e eficiente. Com a tecnologia ao nosso lado, facilitamos o agendamento de consultas e a organização de pacientes e médicos, proporcionando mais praticidade para você. Cuidar do seu bem-estar nunca foi tão simples!</p>
+    </div>
+
+    
+  );
 }
