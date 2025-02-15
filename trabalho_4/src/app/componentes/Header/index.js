25b7ba28a0293226129fc19fda9eaf354f438d7c
anacaroline16.github.io <caroline.cordeiro@estudante.ifro.edu.br>
2025-02-15 14:17:46 -0300

    atualização 15/02

--- trabalho_4/src/app/componentes/Header/index.js
+++ trabalho_4/src/app/componentes/Header/index.js
@@ -2,56 +2,94 @@
 import Link from "next/link";
 import style from "./header.module.css";
 import Image from "next/image";
+import { useState} from "react";
 // import { FaUserDoctor, FaHospitalUser, FaCalendarAlt, FaHome } from "react-icons/fa";
 
 
 
 
 export default function Header(){
+    const [aberto, setAberto] = useState("");
+
+    const abrirSubMenu = (submenu) =>{
+        setAberto(submenu);
+    };
+    
+    const fecharSubMenu = () => {
+        setAberto("");
+    }
+    const submenus = {
+        medicos: [
+            {Link: "/medico", label:"Listar"},
+            {Link: "#", label:"Adicionar"},
+            {Link: "#", label:"Editar"},
+            {Link: "#", label:"Excluir"}
+        ],
+        pacientes: [
+            {Link: "/pacientes", label:"Listar"},
+            {Link: "#", label:"Adicionar"},
+            {Link: "#", label:"Editar"},
+            {Link: "#", label:"Excluir"}
+        ],
+        consultas: [
+            {Link: "/consultas", label:"Listar"},
+            {Link: "#", label:"Adicionar"},
+            {Link: "#", label:"Editar"},
+            {Link: "#", label:"Excluir"}
+        ]
+    };
+    
     return(
         <header className={style.header}>
             <div className={style.divMenu}>
                 <div className={style.divLogo}>
                     <Image className={style.imagemLogo} src='/imagens/CliniSys_transparent-.png' alt="Logo da pagina" width={100} height={70}/>
                 </div>
-                <nav className={style.nav}>
+                <nav className={style.nav} onMouseLeave={fecharSubMenu}>
                     <ul className={style.ul} >
                             <li className={style.li}>
                                 <Link className={style.link} href='/#' > 
                                 Home</Link>
                             </li>
-                            <li className={style.li}>
-                                <Link className={style.link}  href='#'> 
-                                  Medicos </Link> 
-                                <ul>
-                                    <li><Link href="/medico"> Listar</Link></li>
-                                    <li><Link href="#"> Adicionar</Link></li>
-                                    <li><Link href="#"> Editar</Link></li>
-                                    <li><Link href="#"> Excluir</Link></li>
-                                </ul>  
+                            <li className={style.li} onMouseOver={() => abrirSubMenu("medicos")}>
+                                <Link className={style.link}  href='#'>Medicos </Link> 
+                                {aberto === "medicos" && (
+                                    <ul className={style.ulSubMenu}>
+                                        {submenus.medicos.map((item, index) =>(
+                                            <li key={index} className={style.liSubMenu}>
+                                                <Link href={item.Link}> {item.label}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
                             </li>
-                            
-                            <li className={style.li}>
-                                <Link className={style.link} href='#' > 
-                                Paciente</Link>
-                                <ul>
-                                    <li><Link href="/pacientes"> Listar</Link></li>
-                                    <li><Link href="#"> Adicionar</Link></li>
-                                    <li><Link href="#"> Editar</Link></li>
-                                    <li><Link href="#"> Excluir</Link></li>
-                                </ul> 
+                            <li className={style.li} onMouseOver={() => abrirSubMenu("pacientes")}>
+                            <Link className={style.link}  href='#'>Pacientes </Link> 
+                                {aberto === "pacientes" && (
+                                    <ul className={style.ulSubMenu}>
+                                        {submenus.pacientes.map((item, index) =>(
+                                            <li key={index} className={style.liSubMenu}>
+                                                <Link href={item.Link}> {item.label}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                                
                             </li>
-                            <li className={style.li}>
-                                <Link className={style.link} href='#' >
-                                Consultas</Link>
-                                <ul>
-                                    <li><Link href="/consultas"> Listar</Link></li>
-                                    <li><Link href="#"> Adicionar</Link></li>
-                                    <li><Link href="#"> Editar</Link></li>
-                                    <li><Link href="#"> Excluir</Link></li>
-                                </ul> 
-                            </li>
-                    </ul>
+                            <li className={style.li} onMouseOver={() => abrirSubMenu("consultas")}>
+                            <Link className={style.link}  href='#'>Consultas </Link> 
+                                {aberto === "consultas" && (
+                                    <ul className={style.ulSubMenu}>
+                                        {submenus.consultas.map((item, index) =>(
+                                            <li key={index} className={style.liSubMenu}>
+                                                <Link href={item.Link}> {item.label}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                                
+                            </li>    
+                    </ul>  
                 </nav>
             </div>
         </header>
